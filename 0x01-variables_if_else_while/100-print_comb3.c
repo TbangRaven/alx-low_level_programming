fb68c6f83598640bfdfbf972419b251bd3e15507
TbangRaven <thabangmokoena910@gmail.com>
2023-08-14 14:00:52 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+/**
+ * main - program prints all possible different combination of two digits
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int ones = '0';
+	int tens = '0';
+
+	for (tens = '0'; tens <= '9'; tens++)
+	{
+		for (ones = '0'; ones <= '9'; ones++)
+		{
+			if (!((ones == tens) || (tens >  '8')))
+			{
+				putchar(tens);
+				putchar(ones);
+				if (!(ones == '9' && tens == '8'))
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
