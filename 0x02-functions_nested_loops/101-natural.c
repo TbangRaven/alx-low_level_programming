eeab0d1e073bd545e24bfe9848b7d16e984d2edb
TbangRaven <thabangmokoena910@gmail.com>
2023-08-16 19:47:20 -0700

    101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -12,24 +12,14 @@
  */
 int main(void)
 {
-	unsigned long int sum3, sum5, sum;
-	int i;
+	int i, sum = 0;
 
-	sum3 = 0;
-	sum5 = 0;
-	sum = 0;
-
-	for (i = 0; i < 1024; ++i)
+	for (i = 0; i < 1024; i++)
 	{
-		if ((i % 3) == 0)
-		{
-			sum3 = sum3 + i;
-		} else if ((i % 5) == 0)
-		{
-			sum5 = sum 5 + i;
-		}
+		if ((i % 3) == 0 || (i % 5) == 0)
+			sum += i;
 	}
-	sum = sum3 + sum5;
-	printf("%lu\n", sum);
+
+	prinf("%d\n", sum);
 	return (0);
 }
